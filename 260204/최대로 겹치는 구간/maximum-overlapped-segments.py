1d89d30516521adae5d199652ed3bad71383d99b
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-04 08:35:19 +0900

    Title: 최대로 겹치는 구간, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260204/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 260204/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -5,6 +5,6 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 nl = [0] * 201
 
 for start, end in segments:
-    for i in range(start+100, end+101):
+    for i in range(start+100, end+100):
         nl[i] += 1
 print(max(nl))
