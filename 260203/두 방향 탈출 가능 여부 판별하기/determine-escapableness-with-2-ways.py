20ac8b9e7f86559e968406d13ca6a8112431f1f8
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-03 17:18:31 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 260203/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 260203/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -6,14 +6,18 @@ grid = [list(map(int, input().split())) for _ in range(N)]
 from collections import deque
 
 q = deque([(0, 0)])
+visited = [[False] * (M) for _ in range(N)]
+visited[0][0] = True
 is_find = 0
 while q:
     row, col = q.pop()
     if (row, col) == (N-1, M-1):
         is_find = 1
         break
-    if 0 <= row+1 < N and grid[row+1][col]:
+    if 0 <= row+1 < N and grid[row+1][col] and not visited[row+1][col]:
         q.append((row+1, col))
-    if 0 <= col+1 < M and grid[row][col+1]:
+        visited[row+1][col] = True
+    if 0 <= col+1 < M and grid[row][col+1] and not visited[row][col+1]:
         q.append((row, col+1))
+        visited[row][col+1] = True
 print(is_find)
