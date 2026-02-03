5dcdfaff69158af18e9c6b10b68fde9cc97ad6cd
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-03 14:12:54 +0900

    Title: 겹치지 않게 선분 고르기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260203/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 260203/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -4,13 +4,13 @@ line_list = []
 for _ in range(n):
     line_list.append(tuple(map(int, input().split())))
 line_list.sort(key = lambda x: x[1])
-
 # Please write your code here.
 
 line = [False] * 1001
 count = 0
 for A, B in line_list:
     if not line[A] and not line[B]:
+        print(A, B)
         for i in range(A, B+1):
             line[i] = True
         count += 1
