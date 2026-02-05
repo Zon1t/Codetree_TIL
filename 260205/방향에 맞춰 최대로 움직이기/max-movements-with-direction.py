3106121df4ba6f208d626c34dc34cebb9dd9fd68
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-05 16:07:12 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260205/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -0,0 +1,43 @@
+n = int(input())
+num = [list(map(int, input().split())) for _ in range(n)]
+move_dir = [list(map(int, input().split())) for _ in range(n)]
+r, c = map(int, input().split())
+
+# Please write your code here.
+
+def find_bigger(row, col):
+    dr, dc = direction_dict[move_dir[row][col]]
+    possible_list = []
+    nr, nc = row + dr, col + dc
+    while 0 <= nr < n and 0 <= nc < c and num[row][col] < num[nr][nc]:
+        possible_list.append((nr, nc))
+        nr += dr
+        nc += dc
+    return possible_list
+
+def backtrack(row, col, count):
+    global max_cnt
+    possible_list = find_bigger(row, col)
+
+    if not possible_list:
+        max_cnt = count if max_cnt < count else max_cnt
+        return
+    
+    for next_row, next_col in possible_list:
+        backtrack(next_row, next_col, count+1)
+
+direction_dict = {
+    1 : [-1, 0],
+    2 : [-1, 1],
+    3 : [0, 1],
+    4 : [1, 1],
+    5 : [1, 0],
+    6 : [1, -1],
+    7 : [0, -1],
+    8 : [-1, -1]
+}
+max_cnt = 0
+for row in range(n):
+    for col in range(n):
+        backtrack(row, col, 1)
+print(max_cnt)
