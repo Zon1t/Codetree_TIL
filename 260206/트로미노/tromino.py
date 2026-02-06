682a5c096ac7a0c3e2eb1ac35a5f6d0ea9b8e0cd
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-06 09:06:32 +0900

    Title: 트로미노, Time: 87ms, Memory: 18MB, Status: Passed - Codetree

--- 260206/트로미노/tromino.py
+++ 260206/트로미노/tromino.py
@@ -5,15 +5,29 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 max_num = 0
 
-for row in range(n-1):
-    for col in range(m-1):
-        temp = 0
-        temp += grid[row][col] + grid[row+1][col] + grid[row+1][col+1]
-        max_num = max(max_num, temp)
+drc_dict = {
+    0 : [(0, 0), (1, 0), (1, 1)],
+    1 : [(1, 0), (1, 1), (0, 1)],
+    2 : [(0, 0), (0, 1), (1, 1)],
+    3 : [(0, 0), (1, 0), (0, 1)],
+    4 : [(0, 0), (0, 1), (0, 2)],
+    5 : [(0, 0), (1, 0), (2, 0)]
+}
+
+def calc_area(drc_list, row, col):
+    temp = 0
+    for dr, dc in drc_list:
+        nr, nc = row + dr, col + dc
+        if 0 <= nr < n and 0 <= nc < m:
+            temp += grid[nr][nc]
+        else:
+            return 0
+    return temp
 
 for row in range(n):
-    for col in range(m-2):
-        temp = 0
-        temp += grid[row][col] + grid[row][col+1] + grid[row][col+2]
-        max_num = max(max_num, temp)
+    for col in range(m):
+        for i in range(6):
+            temp = calc_area(drc_dict[i], row, col)
+            max_num = max(max_num, temp)
+
 print(max_num)
