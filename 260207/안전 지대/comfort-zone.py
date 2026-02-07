94a6b00e6f597a70071a70e460111b513090efe4
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-07 22:35:12 +0900

    Title: 안전 지대, Time: 54ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260207/안전 지대/comfort-zone.py
@@ -0,0 +1,39 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+from collections import deque
+
+dr = [0, -1, 0, 1]
+dc = [1, 0, -1, 0]
+
+def find_safe_zone(row, col):
+    q = deque([(row, col)])
+    visited[row][col] = True
+    while q:
+        r, c = q.popleft()
+        for i in range(4):
+            nr, nc = r+dr[i], c+dc[i]
+            if 0 <= nr < n and 0 <= nc < m and not visited[nr][nc] and grid[nr][nc] != 0:
+                q.append((nr, nc))
+                visited[nr][nc] = True
+
+max_count = 0
+max_count_height = 0
+
+for h in range(1, 101):
+    for row in range(n):
+        for col in range(m):
+            if grid[row][col] <= h:
+                grid[row][col] = 0
+    temp = 0
+    visited = [[False]*m for _ in range(n)]
+    for row in range(n):
+        for col in range(m):
+            if grid[row][col] and not visited[row][col]:
+                find_safe_zone(row, col)
+                temp += 1
+    if max_count < temp:
+        max_count = temp
+        max_count_height = h
+print(max_count_height, max_count)
