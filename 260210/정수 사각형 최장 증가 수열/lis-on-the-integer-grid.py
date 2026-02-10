37ac959848057b16d2f121985a32c20e37e46690
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-10 12:05:53 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 292ms, Memory: 31MB, Status: Passed - Codetree

--- 260210/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 260210/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -14,25 +14,19 @@ dc = [1, 0, -1, 0]
 def in_range(row, col):
     return (0 <= row < n and 0 <= col < n)
 
-def update(row, col, start):
-    q = deque([(row, col, start)])
-    while q:
-        r, c, d = q.popleft()
-        answer[r][c] = d
-        for i in range(4):
-            nr, nc = r+dr[i], c+dc[i]
-            if in_range(nr, nc) and grid[r][c] < grid[nr][nc]:
-                if answer[nr][nc] == 0:
-                    answer[nr][nc] = d+1
-                    q.append((nr, nc, d+1))
-                else:
-                    if answer[r][c] >= answer[nr][nc]:
-                        update(nr, nc, d+1)
-                        break
+def update(row, col):
+    if answer[row][col]:
+        return answer[row][col]
+    temp = 1
+    for i in range(4):
+        nr, nc = row+dr[i], col+dc[i]
+        if in_range(nr, nc) and grid[row][col] < grid[nr][nc]:
+            temp = max(temp, update(nr, nc) + 1)
+    answer[row][col] = temp
+    return temp
 
 answer = [[0]*n for _ in range(n)]
 for row in range(n):
     for col in range(n):
-        if answer[row][col] == 0:
-            update(row, col, 1)
+        update(row, col)
 print(max([max(row) for row in answer]))
