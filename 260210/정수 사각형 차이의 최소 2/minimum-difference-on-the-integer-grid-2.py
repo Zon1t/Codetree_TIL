6bcc76e8019dc40cefe5e0d9c0137e2f42a93411
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-10 13:12:06 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260210/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 260210/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -4,8 +4,8 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 
 answer_list = [[[0]*2 for _ in range(n)] for _ in range(n)]
-answer_list[0][0][0] = 1
-answer_list[0][0][1] = 1
+answer_list[0][0][0] = grid[0][0]
+answer_list[0][0][1] = grid[0][0]
 
 for i in range(1, n):
     answer_list[0][i][0] = min(answer_list[0][i-1][0], grid[0][i])
@@ -25,4 +25,5 @@ for row in range(1, n):
         else:
             answer_list[row][col][0] = temp_min2
             answer_list[row][col][1] = temp_max2
+
 print(answer_list[-1][-1][1]-answer_list[-1][-1][0])
