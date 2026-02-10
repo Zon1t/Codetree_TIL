368e11a108d2e76abcb4c26305097df37f62647c
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-10 13:08:03 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260210/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -0,0 +1,28 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+answer_list = [[[0]*2 for _ in range(n)] for _ in range(n)]
+answer_list[0][0][0] = 1
+answer_list[0][0][1] = 1
+
+for i in range(1, n):
+    answer_list[0][i][0] = min(answer_list[0][i-1][0], grid[0][i])
+    answer_list[0][i][1] = max(answer_list[0][i-1][1], grid[0][i])
+    answer_list[i][0][0] = min(answer_list[i-1][0][0], grid[i][0])
+    answer_list[i][0][1] = max(answer_list[i-1][0][1], grid[i][0])
+
+for row in range(1, n):
+    for col in range(1, n):
+        temp_min1 = min(answer_list[row-1][col][0], grid[row][col])
+        temp_max1 = max(answer_list[row-1][col][1], grid[row][col])
+        temp_min2 = min(answer_list[row][col-1][0], grid[row][col])
+        temp_max2 = max(answer_list[row][col-1][1], grid[row][col])
+        if temp_max1 - temp_min1 < temp_max2 - temp_min2:
+            answer_list[row][col][0] = temp_min1
+            answer_list[row][col][1] = temp_max1
+        else:
+            answer_list[row][col][0] = temp_min2
+            answer_list[row][col][1] = temp_max2
+print(answer_list[-1][-1][1]-answer_list[-1][-1][0])
