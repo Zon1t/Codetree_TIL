d4f326350484731cc71b917b3b55bc4cc53d06b5
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-12 16:24:54 +0900

    Title: 2차원 최대 증가 수열, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 260212/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 260212/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -3,14 +3,14 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 
-dp = [[0]*n for _ in range(n)]
+dp = [[0] * m for _ in range(n)]
 dp[0][0] = 1
 
 for row in range(1, n):
-    for col in range(1, n):
+    for col in range(1, m):
         for i in range(row):
             for j in range(col):
                 if grid[row][col] > grid[i][j] and dp[i][j] != 0:
-                    dp[row][col] = max(dp[i][j] + 1, dp[row][col])
+                    dp[row][col] = max(dp[i][j]+1, dp[row][col])
 
 print(max([max(row) for row in dp]))
