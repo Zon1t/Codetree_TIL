71daabea9243637e581175a694cabaf196db2c85
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-13 09:39:06 +0900

    Title: 동전 거슬러주기, Time: 122ms, Memory: 22MB, Status: Runtime Error - Codetree

--- 260213/동전 거슬러주기/coin-change.py
+++ 260213/동전 거슬러주기/coin-change.py
@@ -6,14 +6,28 @@ coins = list(map(int, input().split()))
 import sys
 INT_MAX = sys.maxsize
 
-dp = [INT_MAX] * (M+1)
-dp[0] = 0
+memo = [-1] * (M+1)
 
-for i in range(1, M+1):
+def get_min_cnt(num):
+    if memo[num] != -1:
+        return memo[num]
+    
+    if num == M:
+        memo[num] = 0
+        return 0
+    
+    min_cnt = INT_MAX
+
     for coin in coins:
-        if coin <= i:
-            dp[i] = min(dp[i], dp[i-coin]+1)
-if dp[M] == INT_MAX:
+        if num + coin <= M:
+            min_cnt = min(min_cnt, get_min_cnt(num+coin)+1)
+    
+    memo[num] = min_cnt
+    return min_cnt
+
+answer = get_min_cnt(0)
+
+if answer == INT_MAX:
     print(-1)
 else:
-    print(dp[M])
+    print(answer)
