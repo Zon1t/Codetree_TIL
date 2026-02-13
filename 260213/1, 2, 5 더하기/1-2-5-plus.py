94a016fd48eeffb68a26f892f9dc4ebf9366501c
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-13 15:24:11 +0900

    Title: 1, 2, 5 더하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260213/1, 2, 5 더하기/1-2-5-plus.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+# Please write your code here.
+
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 2
+dp[3] = 3
+dp[4] = 5
+dp[5] = 9
+for i in range(6, n+1):
+    dp[i] = (dp[i-1] + dp[i-2] + dp[i-5])%10007
+print(dp[n])
