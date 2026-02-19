887801736c68e862c386120ab87c2a9632213890
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-20 08:33:21 +0900

    Title: 최대 동전 거슬러주기, Time: 85ms, Memory: 29MB, Status: Runtime Error - Codetree

--- 260220/최대 동전 거슬러주기/max-coin-change.py
+++ 260220/최대 동전 거슬러주기/max-coin-change.py
@@ -7,7 +7,7 @@ dp = [0] * (M+1)
 
 for i in range(1, M+1):
     for coin in coins:
-        dp[coin] = 1
+        dp[coin] = max(1, dp[coin])
         if coin <= i and dp[i-coin] != 0:
             dp[i] = max(dp[i-coin]+1, dp[i])
 print(dp[M])
