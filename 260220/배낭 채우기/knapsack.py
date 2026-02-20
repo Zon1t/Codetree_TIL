26768512f5b44d99c531f23f0b889223fbbff052
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-20 10:36:56 +0900

    Title: 배낭 채우기, Time: 97ms, Memory: 25MB, Status: Passed - Codetree

--- 260220/배낭 채우기/knapsack.py
+++ 260220/배낭 채우기/knapsack.py
@@ -1,17 +1,27 @@
 N, M = map(int, input().split())
 w, v = zip(*[tuple(map(int, input().split())) for _ in range(N)])
-w, v = list(w), list(v)
+w, v = [0]+list(w), [0]+list(v)
 
 # Please write your code here.
 
-dp = [[0] * (M+1) for _ in range(N)]
-for i in range(N):
-    for j in range(M, 0, -1):
-        if w[i] <= j:
-            dp[i][j] = max(dp[i-1][j-w[i]] + v[i], dp[i][j-1], dp[i-1][j])
+# dp = [[0] * (M+1) for _ in range(N)]
+# for i in range(N):
+#     for j in range(M, 0, -1):
+#         if w[i] <= j:
+#             dp[i][j] = max(dp[i-1][j-w[i]] + v[i], dp[i][j-1], dp[i-1][j])
+#         else:
+#             if i > 0:
+#                 dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+#             else:
+#                 dp[i][j] = dp[i][j-1]
+# print(dp[-1][-1])
+
+dp = [[-1] * (M+1) for _ in range(N+1)]
+dp[0][0] = 0
+for i in range(1, N+1):
+    for j in range(M, -1, -1):
+        if j < w[i]:
+            dp[i][j] = dp[i-1][j]
         else:
-            if i > 0:
-                dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-            else:
-                dp[i][j] = dp[i][j-1]
-print(dp[-1][-1])
+            dp[i][j] = max(dp[i-1][j-w[i]]+v[i], dp[i-1][j])
+print(max(dp[N]))
