455749ca7c93cd7d3cde86462485810788e9014a
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-20 11:30:36 +0900

    Title: 배낭 채우기 2, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 260220/배낭 채우기 2/knapsack-2.py
+++ 260220/배낭 채우기 2/knapsack-2.py
@@ -4,11 +4,18 @@ w, v = [0]+list(w), [0]+list(v)
 
 # Please write your code here.
 
-dp = [[0] * (M+1) for _ in range(N+1)]
+# 굳이 이렇게 풀 이유가 없었다.
+# dp = [[0] * (M+1) for _ in range(N+1)]
+# for i in range(1, N+1):
+#     for j in range(1, M+1):
+#         if j >= w[i]:
+#             dp[i][j] = max(dp[i-1][j], dp[i][j-w[i]]+v[i])
+#         else:
+#             dp[i][j] = dp[i-1][j]
+# print(dp[-1][-1])
+
+dp = [0] * (M+1)
 for i in range(1, N+1):
-    for j in range(1, M+1):
-        if j >= w[i]:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-w[i]]+v[i])
-        else:
-            dp[i][j] = dp[i-1][j]
-print(dp[-1][-1])
+    for j in range(w[i], M+1):
+        dp[j] = max(dp[j-w[i]]+v[i], dp[j])
+print(max(dp))
