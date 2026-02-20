c0b946823cf25dfc997efa7f1aea6196bc1f75ba
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-20 11:26:01 +0900

    Title: 배낭 채우기 2, Time: 64ms, Memory: 18MB, Status: Passed - Codetree

--- 260220/배낭 채우기 2/knapsack-2.py
+++ 260220/배낭 채우기 2/knapsack-2.py
@@ -6,7 +6,7 @@ w, v = [0]+list(w), [0]+list(v)
 
 dp = [[0] * (M+1) for _ in range(N+1)]
 for i in range(1, N+1):
-    for j in range(w[i], M+1):
+    for j in range(1, M+1):
         if j >= w[i]:
             dp[i][j] = max(dp[i-1][j], dp[i][j-w[i]]+v[i])
         else:
