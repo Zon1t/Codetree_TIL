0f8e59cc77c89f27a01e5f8820267f9f824080c2
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-02-20 10:10:46 +0900

    Title: 배낭 채우기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260220/배낭 채우기/knapsack.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+w, v = zip(*[tuple(map(int, input().split())) for _ in range(N)])
+w, v = list(w), list(v)
+
+# Please write your code here.
+
+dp = [[0] * (M+1) for _ in range(N)]
+for i in range(N):
+    for j in range(w[i], M+1):
+        dp[i][j] = max(dp[i-1][j-w[i]] + v[i], dp[i][j-1])
+print(dp[-1][-1])
