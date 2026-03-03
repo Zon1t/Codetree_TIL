2f37220ebccda8144c8f2fadce6bf2a8cba96979
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-03-03 11:33:36 +0900

    Title: 두 수의 합, Time: 77ms, Memory: 26MB, Status: Passed - Codetree

--- 260303/두 수의 합/sum-of-two-num.py
+++ 260303/두 수의 합/sum-of-two-num.py
@@ -4,11 +4,15 @@ arr = list(map(int, input().split()))
 # Please write your code here.
 
 store_dict = dict()
+count = 0
 
 for num in arr:
-    key_list = list(store_dict.keys())
-    for key in key_list:
-        store_dict[key+num] = store_dict.get(key+num, 0) + store_dict[key]
     store_dict[num] = store_dict.get(num, 0) + 1
 
-print(store_dict[k])
+for key in store_dict:
+    count += store_dict[key] * store_dict.get(k-key, 0)
+
+if not k%2 and (k//2 in store_dict):
+    count -= store_dict[k//2]
+
+print(count//2)
