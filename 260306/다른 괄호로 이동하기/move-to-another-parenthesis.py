ba3fa548016db1ee5db41eefbbbe15e9f535c543
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-03-06 10:29:55 +0900

    Title: 다른 괄호로 이동하기, Time: 1719ms, Memory: 28MB, Status: Passed - Codetree

--- 260306/다른 괄호로 이동하기/move-to-another-parenthesis.py
+++ 260306/다른 괄호로 이동하기/move-to-another-parenthesis.py
@@ -5,39 +5,37 @@ N, A, B = map(int, input().split())
 total_num = N**2
 grid = [input().strip() for _ in range(N)]
 
-distances = [[MAX_VALUE] * (total_num) for _ in range(total_num)]
-for i in range(total_num):
-    distances[i][i] = 0
-    i_row, i_col = i//N, i%N
-    i_val = grid[i_row][i_col]
-    for j in range(i+1, total_num):
-        j_row, j_col = j//N, j%N
-        j_val = grid[j_row][j_col]
-        if i_row == j_row:
-            if j_col - i_col == 1:
-                if i_val == j_val:
-                    distances[i][j] = A
-                    distances[j][i] = A
-                else:
-                    distances[i][j] = B
-                    distances[j][i] = B
-        elif i_col == j_col:
-            if j_row - i_row == 1:
-                if i_val == j_val:
-                    distances[i][j] = A
-                    distances[j][i] = A
-                else:
-                    distances[i][j] = B
-                    distances[j][i] = B
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
 
-for k in range(total_num):
-    for i in range(total_num):
-        for j in range(total_num):
-            distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j])
+def in_range(row, col):
+    return (0 <= row < N and 0 <= col < N)
+
+def dijkstra(row, col):
+    costs = [[MAX_VALUE]*N for _ in range(N)]
+    costs[row][col] = 0
+    Q = [(0, row, col)]
+    while Q:
+        curr_cost, curr_row, curr_col = heapq.heappop(Q)
+        curr_value = grid[curr_row][curr_col]
+        if curr_cost > costs[curr_row][curr_col]:
+            continue
+        for i in range(4):
+            next_row, next_col = curr_row + dr[i], curr_col + dc[i]
+            if not in_range(next_row, next_col):
+                continue
+            next_value = grid[next_row][next_col]
+            cost = A if curr_value == next_value else B
+            next_cost = curr_cost + cost
+            if next_cost < costs[next_row][next_col]:
+                costs[next_row][next_col] = next_cost
+                heapq.heappush(Q, (next_cost, next_row, next_col))
+    return max([max(row) for row in costs])
 
 answer = 0
-for i in range(total_num):
-    for j in range(i+1, total_num):
-        if answer < distances[i][j]:
-            answer = distances[i][j]
+for row in range(N):
+    for col in range(N):
+        now_max = dijkstra(row, col)
+        answer = now_max if answer < now_max else answer
+
 print(answer)
