69f801323f9c79362539034c9fff7498d7bb6c18
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-04-02 08:24:53 +0900

    Title: 변수 값 복사하기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 1;
+    int b = 5;
+    int c = 3;
+    a = c + c;
+    b = b - c;
+    cout << a;
+    cout << '\n';
+    cout << b;
+    cout << '\n';
+    cout << c;
+    return 0;
+}
