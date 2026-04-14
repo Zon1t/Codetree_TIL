93848c9fe19c59a2708224fc2777338a61229b76
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-04-14 10:47:10 +0900

    Title: 시험 통과 여부 확인하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score;
+
+    cin >> score;
+
+    if(score >= 80){
+        cout << "pass" << endl;
+    }
+    else {
+        cout << 80-score << " more score" << endl;
+    }
+    return 0;
+}
