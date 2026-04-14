4ed0fa73db0ac55b7f03be0d7793170413a91c93
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-04-14 09:01:08 +0900

    Title: 실수 받아 그대로 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double a;
+
+    cin >> a;
+    cout << fixed;
+    cout << setprecision(2);
+    cout << a;
+
+    return 0;
+}
