f0c3ef92efb3cd74e30c1c9d11b553903ea5e10f
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-04-15 11:11:03 +0900

    Title: 약수가 세 개인 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260415/약수가 세 개인 수/numbers-with-three-divisors.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int start, end, temp, cnt = 0;
+    cin >> start >> end;
+
+    for (int i = start; i <= end; i++) {
+        temp = 0;
+        for (int j = 1; j <= i; j++) {
+            if (i%j == 0) {
+                temp++;
+            }
+        }
+        if (temp == 3) {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
