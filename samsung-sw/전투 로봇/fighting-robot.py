db9e5600b2c3d9b897e9f9f184742e69f64275b0
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-08-24 20:27:56 +0900

    [Passed] 전투 로봇 (58ms, 17MB)

--- samsung-sw/전투 로봇/fighting-robot.py
+++ samsung-sw/전투 로봇/fighting-robot.py
@@ -1,63 +1,58 @@
-# 뇌절해보자 : 비트마스킹 + heapq
-
-from heapq import heappop, heappush
-
-def kill_check(cur, cur_Lv):
-    q = []
-    # (거리, x좌표, y좌표)
-    heappush(q, (0, cur))
-
-    visited = 1<<cur
-
-    while q:
-        dist, now = heappop(q)
-
-        monster_Lv = matrix[now]
-
-        if 0 < monster_Lv < cur_Lv:
-            return (dist, now, monster_Lv)
-
-        for d in dxdy:
-            nxt = now + d
-            if (d==1 and nxt%N==0) or (d==-1 and nxt%N==N-1):
-                continue
-            if 0 <= nxt < N**2:
-                if not (visited&(1 << nxt)) and matrix[nxt] <= cur_Lv:
-                    visited |= (1 << nxt)
-                    heappush(q, (dist + 1, nxt))
-
-    return (-1, -1, -1)
+from collections import deque
 
 N = int(input())
-dxdy = (1, -1, N, -N)
+end = N**2
+deltas = (1, -1, N, -N)
 
-matrix = []
+pos = []
+curr_pos = -1
 for i in range(N):
-    matrix.extend(list(map(int, input().split())))
+    temp = list(map(int, input().split()))
+    if curr_pos == -1:
+        for j in range(N):
+            if temp[j] == 9:
+                curr_pos = i*N+j
+                temp[j] = 0
+    pos.extend(temp)
 
-cur = -1
-for j, val in enumerate(matrix):
-    if val == 9:
-        cur = j
-        matrix[j] = 0 # 이거 안해주면 나중에 못감
-        break
+def bfs():
+    visited = 1<<curr_pos
+    time_ = 0
+    monsters = []
+    Q = deque()
+    Q.append((0, curr_pos))
+    while Q:
+        now_time, now_pos = Q.popleft()
+        if time_ != now_time:
+            if monsters:
+                monsters.sort()
+                return monsters[0], now_time
+            time_ = now_time
+        for d in deltas:
+            next_pos = now_pos + d
+            if not (0 <= next_pos < end) or (visited & 1<<next_pos):
+                continue
+            if (d==1 and next_pos%N==0) or (d==-1 and next_pos%N==N-1):
+                continue
+            if pos[next_pos] > level:
+                continue
+            elif 0 < pos[next_pos] < level:
+                monsters.append(next_pos)
+            visited |= 1 << next_pos
+            Q.append((now_time+1, next_pos))
+    return -1, -1
 
-cur_Lv = 2
-cur_exp = 0
-time = 0
-
+time, cnt, level = 0, 0, 2
 while True:
-    dist, nxt, monster_Lv = kill_check(cur, cur_Lv)
-
-    if dist == -1:
+    next_pos, delta = bfs()
+    if next_pos == -1:
         break
 
-    time += dist            # 죽임
-    matrix[nxt] = 0
-    cur_exp += 1
-    if cur_exp == cur_Lv:   # 레벨업
-        cur_Lv += 1
-        cur_exp = 0
-    cur = nxt               # 이동
-
+    curr_pos = next_pos
+    pos[curr_pos] = 0
+    time += delta
+    cnt += 1
+    if cnt == level:
+        level += 1
+        cnt = 0
 print(time)
