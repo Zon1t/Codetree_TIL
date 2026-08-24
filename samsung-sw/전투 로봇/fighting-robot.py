02709d1dfd1e8661d34551fb1ddfac9d933ce27c
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-08-24 20:19:20 +0900

    [Passed] 전투 로봇 (51ms, 16MB)

--- samsung-sw/전투 로봇/fighting-robot.py
+++ samsung-sw/전투 로봇/fighting-robot.py
@@ -5,19 +5,18 @@ from heapq import heappop, heappush
 def kill_check(cur, cur_Lv):
     q = []
     # (거리, x좌표, y좌표)
-    heappush(q, (0, cur//N, cur%N))
+    heappush(q, (0, cur))
 
     visited = 0
     visited |= 1<<cur
 
     while q:
-        dist, cx, cy = heappop(q)
-        now = cx*N + cy
+        dist, now = heappop(q)
 
         monster_Lv = matrix[now]
 
         if 0 < monster_Lv < cur_Lv:
-            return (dist, cx, cy, monster_Lv, now)
+            return (dist, now, monster_Lv)
 
         for d in dxdy:
             nxt = now + d
@@ -26,9 +25,9 @@ def kill_check(cur, cur_Lv):
             if 0 <= nxt < N**2:
                 if not (visited&(1 << nxt)) and matrix[nxt] <= cur_Lv:
                     visited |= (1 << nxt)
-                    heappush(q, (dist + 1, nxt//N, nxt%N))
+                    heappush(q, (dist + 1, nxt))
 
-    return (-1, -1, -1, -1, -1)
+    return (-1, -1, -1)
 
 N = int(input())
 dxdy = (1, -1, N, -N)
@@ -48,7 +47,7 @@ cur_exp = 0
 time = 0
 
 while True:
-    dist, nx, ny, monster_Lv, nxt = kill_check(cur, cur_Lv)
+    dist, nxt, monster_Lv = kill_check(cur, cur_Lv)
 
     if dist == -1:
         break
