8de667a94bc705ab757a88d80218a5cb13e1d7c1
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-08-25 15:34:46 +0900

    [Passed] 이상한 체스 (103ms, 19MB)

--- samsung-sw/이상한 체스/odd-chess.py
+++ samsung-sw/이상한 체스/odd-chess.py
@@ -1,8 +1,11 @@
-# 백트래킹 풀이
+# 백트래킹 문제인가? 격자의 크기는 최대 8*8
+# 자신의 말도 최대 8개. 음.. 4^8의 가짓수면 그냥 완탐이긴 하다.
+# 뭘 어떻게 풀어도 다 될 것 같긴 한데, 그냥 itertools 써보면 될 것도 같다.
+
+from itertools import product
 
 def in_range(row, col):
     return 0 <= row < N and 0 <= col < M
-
 
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
@@ -13,52 +16,36 @@ deltas = [None, [0], [-1, 1], [0, 1], [-1, 0, 1], [-1, 0, 1, 2]]
 
 N, M = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(N)]
+answer = 64
 
-ours, cnt, empty = [], 0, 0
-fives = []
+cnt, ours = 0, []
+empty = 0
 for row in range(N):
     for col in range(M):
-        if 1 <= grid[row][col] <= 4:
+        if 1 <= grid[row][col] <= 5:
             ours.append((row, col))
             cnt += 1
-        elif grid[row][col] == 5:
-            fives.append((row, col))
         elif grid[row][col] == 0:
             empty += 1
 
-visited = [[0] * M for _ in range(N)]
+comb_lst = []
+for row, col in ours:
+    if grid[row][col] == 2:
+        comb_lst.append((0, 1))
+    elif grid[row][col] == 5:
+        comb_lst.append((0,))
+    else:
+        comb_lst.append((0, 1, 2, 3))
 
-# 5인 애들은 짜피 방향 세팅이 필요 없음.
-for row, col in fives:
-    for d in range(4):
-        next_row, next_col = row, col
-        while True:
-            next_row += dr[d]
-            next_col += dc[d]
-
-            if not in_range(next_row, next_col) or grid[next_row][next_col] == 6:
-                break
-
-            if grid[next_row][next_col] == 0:
-                visited[next_row][next_col] += 1
-                if visited[next_row][next_col] == 1:
-                    empty -= 1
-
-def backtrack(idx, empty):
-    global answer
-
-    if idx == cnt:
-        if empty < answer:
-            answer = empty
-        return
-
-    curr_row, curr_col = ours[idx]
-    curr_num = grid[curr_row][curr_col]
-
-    for d in range(2 if curr_num == 2 else 4):
-        next_empty = empty
-        for delta_dir in deltas[curr_num]:
-            next_dir = (d + delta_dir) % 4
+# 각 조합에 대한 빈 칸의 개수 세기.
+for t in product(*comb_lst):
+    temp_grid = [[0]*M for _ in range(N)]
+    temp_cnt = empty
+    for idx in range(cnt):
+        curr_row, curr_col = ours[idx]
+        curr_dir = t[idx]
+        for delta_d in deltas[grid[curr_row][curr_col]]:
+            next_dir = (curr_dir + delta_d)%4
             next_row, next_col = curr_row, curr_col
             while True:
                 next_row += dr[next_dir]
@@ -67,27 +54,11 @@ def backtrack(idx, empty):
                 if not in_range(next_row, next_col) or grid[next_row][next_col] == 6:
                     break
 
-                if grid[next_row][next_col] == 0:
-                    visited[next_row][next_col] += 1
-                    if visited[next_row][next_col] == 1:
-                        next_empty -= 1
+                if grid[next_row][next_col] == 0 and temp_grid[next_row][next_col] == 0:
+                    temp_cnt -= 1
+                    temp_grid[next_row][next_col] = 1
 
-        backtrack(idx+1, next_empty)
+    if temp_cnt < answer:
+        answer = temp_cnt
 
-        for delta_dir in deltas[curr_num]:
-            next_dir = (d + delta_dir) % 4
-            next_row, next_col = curr_row, curr_col
-            while True:
-                next_row += dr[next_dir]
-                next_col += dc[next_dir]
-
-                if not in_range(next_row, next_col) or grid[next_row][next_col] == 6:
-                    break
-
-                if grid[next_row][next_col] == 0:
-                    visited[next_row][next_col] -= 1
-
-
-answer = empty
-backtrack(0, empty)
 print(answer)
