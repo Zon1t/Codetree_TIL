bcc8bb4474a414e0902c03c4523adcc85accd5d0
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-09-10 11:18:57 +0900

    [Passed] 자율주행 자동차 (61ms, 17MB)

--- samsung-sw/자율주행 자동차/autonomous-driving.py
+++ samsung-sw/자율주행 자동차/autonomous-driving.py
@@ -1,46 +1,38 @@
-# 좌표에 대한 부분은 맞춰서 제시해줌. 방향 북-동-남-서 델타 세팅 맞추기.
-# 이미 방문 / 인도  ->  돌리기, 없으면 후진 후 반복. 후진조차 못하면 종료
-# 첫, 마지막 행과 열은 인도임을 보장. 따로 범위 벗어나는지 체크는 할 필요 없음.
-# 시키는 명령만 수행하면 되므로 while문 돌리면서 수행하면 될 것으로 보인다.
-# 에지라할 수 있는 케이스가 존재하는가?
+# 좌회전 해가며 다음 이동 방향 탐색. 정면 시작 아님!
+# 만약 발견 못했다? 현재 방향 유지하고 후진
+# 후진조차 못한다? 종료
 
-# 북 동 남 서
+# 북동남서
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
+
+# 확인할 방향
+delta_dict = {0: [3, 2, 1, 0],
+              1: [0, 3, 2, 1],
+              2: [1, 0, 3, 2],
+              3: [2, 1, 0, 3]}
+
 
 N, M = map(int, input().split())
 curr_row, curr_col, curr_dir = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(N)]
 
-visited = [[False] * M for _ in range(N)]
-visited[curr_row][curr_col] = True
+visited = [[0] * M for _ in range(N)]
 
 while True:
-
-    # 인접한 네 방향 탐색. 순서는 현재 방향 기준 왼쪽으로 돌면서 체크.
-    for delta in range(1, 5):
-        # 왼쪽으로 도니까 빼주기.
-        next_dir = (curr_dir - delta) % 4
+    visited[curr_row][curr_col] = 1
+    # 1. 이동.
+    for next_dir in delta_dict[curr_dir]:
         next_row, next_col = curr_row + dr[next_dir], curr_col + dc[next_dir]
-
-        # 인도거나 이미 방문한 도로인 경우 왼쪽으로 돌기
-        if grid[next_row][next_col] == 1 or visited[next_row][next_col]:
+        if grid[next_row][next_col] or visited[next_row][next_col]:
             continue
-
-        # 방문할 수 있는 도로면 방향 fix 후 break.
-        curr_dir = next_dir
+        curr_row, curr_col, curr_dir = next_row, next_col, next_dir
         break
-    # for ~ else문. 만약 인접한 네 영역 모두 인도거나 이미 방문했으면 후진 시도를 해야한다.
+    # 2. 이동을 못했을 경우.
     else:
-        # 후진 로직 구성
-        next_row, next_col = curr_row - dr[curr_dir], curr_col - dc[curr_dir]
-
-        # 만약 더 후진할 수 없다면 종료. 인도인지만 체크하면 된다.
-        if grid[next_row][next_col] == 1:
+        curr_row, curr_col = curr_row - dr[curr_dir], curr_col - dc[curr_dir]
+        if grid[curr_row][curr_col]:
             break
 
-    # 방문처리 후 이동.
-    visited[next_row][next_col] = True
-    curr_row, curr_col = next_row, next_col
-
+# 정답 출력
 print(sum([sum(row) for row in visited]))
