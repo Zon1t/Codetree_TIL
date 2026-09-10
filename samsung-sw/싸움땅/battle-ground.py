caf18f9b4ceff1b19c23465c1d526249726da436
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-09-10 16:50:34 +0900

    [Passed] 싸움땅 (127ms, 20MB)

--- samsung-sw/싸움땅/battle-ground.py
+++ samsung-sw/싸움땅/battle-ground.py
@@ -1,105 +1,35 @@
-# 클래스로 풀어보자. 근데 이렇게 하는거 맞음?
-class Player:
-    def __init__(self, i, r, c, d, s, g=0):
-        self.idx = i
-        self.row = r
-        self.col = c
-        self.direction = d
-        self.status = s
-        self.gun = g
+# 총 정렬해두는 버전
 
-    # def __str__(self):
-    #     return f'data:{self.get_data()}, power:{self.get_power()}'
+''' 싸움땅 / 20260910 / 체감 난이도 : 골드 4~3
+소요 시간 : 58분 / 시도 : 1회 / 실행 시간 : 112ms / 메모리 : 19MB
 
-    def get_data(self):
-        return self.row, self.col, self.direction
+타임 라인 : 구상(16분) - 구현(32분) - 검증(10분)
 
-    def get_power(self):
-        return self.status + self.gun
 
-    def update_data(self, r, c, d, update_grid=True):
-        self.row = r
-        self.col = c
-        self.direction = d
-        if update_grid:
-            player_grid[r][c] = self.idx
+[구상]
+    - 굉장히 전형적인 ? '시키는 거 잘해라' 하는 문제라고 생각되었다. 조건이 나름 세세하게 잘 주어져
+    있다고 생각되어서 기본적인 틀만 잡고, 구현 단계에서 문제 읽으며 구현하면 되겠다는 생각이 들었다.
+    - 상호작용을 하기 위해 필요한 변수들을 쭉 생각해보았다. 좌표를 알면 해당 칸 플레이어가 누가 있는
+    지 알아야 하고, 총이 여러개 떨어져 있을 수도 있고 등등.. 조건을 생각하며 변수들을 선언했다.
 
-    # 이동 로직
-    def move(self):
-        curr_row, curr_col, curr_dir = self.get_data()
-        next_row, next_col = curr_row + dr[curr_dir], curr_col + dc[curr_dir]
-        if not in_range(next_row, next_col):
-            curr_dir = (curr_dir + 2) % 4
-            next_row, next_col = curr_row + dr[curr_dir], curr_col + dc[curr_dir]
-        player_grid[curr_row][curr_col] = -1
+[구현]
+    - 그냥 시키는대로 쭉 구현했다. 간만에 함수 거의 안나누고 쭉 하드코딩한 문제였다.
+    - 구현 때 유일하게 까다로웠던 것은 정보 업데이트 시점이였던 것 같다. 업데이트 시점을 잘못 잡으면
+    덮어 씌워질 수 있기 때문에 이를 염두하며 구현을 진행했다. win/lose player 정도만 일반화 작업
+    을 진행했다. 총 줍기 등 일부 로직은 함수화하는 게 더 괜찮았을지도?
 
-        # 1. 사람이 없으면 총줍기 + 격자 업데이트
-        if player_grid[next_row][next_col] == -1:
-            self.update_data(next_row, next_col, curr_dir)
-            self.my_gun()
+[검증]
+    - 주저리주저리 길게 작성한 코드이다 보니 혹시 헷갈린 부분이 있었을까, 잘못 작성한 부분이 있었을까
+    생각해보며 검증을 이어갔다.
+    - pprint와 내가 보고자 하는 변수들을 찍어보며 상태를 관찰해보았다. 모두 올바르게 출력됨을 확인한
+    뒤 문제 한 번 더 읽고 제출해보았다.
+'''
 
-        # 2. 사람이 있으면?
-        else:
-            # 맞짱뜰 사람 잡아오기
-            another_player = players[player_grid[next_row][next_col]]
-            self.update_data(next_row, next_col, curr_dir, False)   # 격자 업뎃은 X
+# 격자 위 여러 객체에 대한 상호작용을 다루는 전형적인 문제같다.
+# 시키는 대로 잘만 하면 될듯? 이럴 때 클래스 쓸 줄 알면 좋은 것 같은데.. 일단은 그냥 하자.
+# 생각해보니 좌표 -> 플레이어도 가능해야 함.. 이것도 저장하자.
 
-            # 뜨자
-            my_power, your_power = self.get_power(), another_player.get_power()
-            if my_power > your_power or (my_power == your_power and self.status > another_player.status):
-                winner, loser = self, another_player
-            else:
-                winner, loser = another_player, self
-
-            # 주어진 로직 수행.
-            scores[winner.idx] += abs(my_power-your_power)
-            loser.urloser()
-            winner.update_data(*winner.get_data())
-            winner.my_gun()
-
-    # 패자가 할 일..
-    def urloser(self):
-        curr_row, curr_col, curr_dir = self.get_data()
-
-        # 총 떨구고
-        if self.gun:
-            guns[curr_row][curr_col].append(self.gun)
-            self.gun = 0
-
-        # 도망가
-        for delta_d in range(4):
-            next_dir = (curr_dir + delta_d) % 4
-            next_row, next_col = curr_row + dr[next_dir], curr_col + dc[next_dir]
-
-            # 격자 밖 or 플레이어 있으면..
-            if not in_range(next_row, next_col) or player_grid[next_row][next_col] != -1:
-                continue
-
-            # 위치가 결정됨.
-            self.update_data(next_row, next_col, next_dir)
-            self.my_gun()
-            break
-
-    # 총 줍자
-    def my_gun(self):
-        curr_row, curr_col, _ = self.get_data()
-
-        # 총이 놓여져 있다면..
-        if guns[curr_row][curr_col]:
-            my_gun, max_gun = self.gun, max(guns[curr_row][curr_col])
-
-            # 클 때만 가져가면 된다.
-            if my_gun < max_gun:
-                max_idx = guns[curr_row][curr_col].index(max_gun)
-
-                # 총을 소지했으면 교체.
-                if my_gun:
-                    guns[curr_row][curr_col][max_idx] = my_gun
-                # 아니면 들고감.
-                else:
-                    guns[curr_row][curr_col].pop(max_idx)
-
-                self.gun = max_gun
+from bisect import insort_left
 
 # 주어진 순서에 맞게 델타 세팅
 dr = [-1, 0, 1, 0]
@@ -110,7 +40,89 @@ def in_range(row, col):
     return 0 <= row < N and 0 <= col < N
 
 
+def move(player):
+    curr_row, curr_col = positions[player]
+    next_dir = directions[player]
+
+    # 1-1. 다음 이동 위치 판단
+    next_row, next_col = curr_row + dr[next_dir], curr_col + dc[next_dir]
+    if not in_range(next_row, next_col):
+        next_dir = (next_dir + 2) % 4
+        next_row, next_col = curr_row + dr[next_dir], curr_col + dc[next_dir]
+
+    player_grid[curr_row][curr_col] = -1
+    positions[player] = (next_row, next_col)
+    directions[player] = next_dir
+
+    # 2-1. 플레이어가 없다면.. 총 줍기.
+    if player_grid[next_row][next_col] == -1:
+        if guns[next_row][next_col]:
+            my_gun, max_gun = having_guns[player], guns[next_row][next_col][-1]
+            if my_gun < max_gun:
+                having_guns[player] = guns[next_row][next_col].pop()
+                if my_gun:
+                    insort_left(guns[next_row][next_col], my_gun)
+
+        # 정보 업데이트 잘하자.
+        player_grid[next_row][next_col] = player
+
+    # 2-2. 만약 플레이어가 있다면?!
+    else:
+        another_player = player_grid[next_row][next_col]
+        my_power, another_power = status[player]+having_guns[player], status[another_player]+having_guns[another_player]
+
+        # 승자 패자 판단하기.
+        if my_power > another_power or \
+                (my_power == another_power and status[player] > status[another_player]):
+            win_player, lose_player = player, another_player
+        else:
+            win_player, lose_player = another_player, player
+
+        scores[win_player] += abs(my_power-another_power)
+
+        # 패배자 총 떨구기
+        if having_guns[lose_player]:
+            insort_left(guns[next_row][next_col], having_guns[lose_player])
+            having_guns[lose_player] = 0
+
+        # 패배자 이동시키기
+        lose_row, lose_col = positions[lose_player]
+        lose_dir = directions[lose_player]
+        for delta_d in range(4):
+            lose_nd = (lose_dir + delta_d) % 4
+            lose_nr, lose_nc = lose_row + dr[lose_nd], lose_col + dc[lose_nd]
+
+            if not in_range(lose_nr, lose_nc) or player_grid[lose_nr][lose_nc] != -1:
+                continue
+
+            # 정보 업데이트
+            lose_row, lose_col, lose_dir = lose_nr, lose_nc, lose_nd
+            player_grid[lose_row][lose_col] = lose_player
+            positions[lose_player] = (lose_row, lose_col)
+            directions[lose_player] = lose_dir
+            break
+
+        # 총 줍기
+        if guns[lose_row][lose_col]:
+            having_guns[lose_player] = guns[lose_row][lose_col].pop()
+
+        # 이긴 애도 총 줍기.
+        if guns[next_row][next_col]:
+            my_gun, max_gun = having_guns[win_player], guns[next_row][next_col][-1]
+            if my_gun < max_gun:
+                having_guns[win_player] = guns[next_row][next_col].pop()
+                if my_gun:
+                    insort_left(guns[next_row][next_col], my_gun)
+
+        # 정보 업데이트
+        player_grid[next_row][next_col] = win_player
+
+# 플레이어 정보 관리
 N, M, K = map(int, input().split())
+positions = []
+directions = []
+status = []
+having_guns = [0] * M          # 초기엔 총 없음
 
 # 총 정보 받기
 guns = [[[] for _ in range(N)] for _ in range(N)]
@@ -122,17 +134,18 @@ for row in range(N):
 
 # 플레이어 정보 받기
 player_grid = [[-1] * N for _ in range(N)]
-players = []
 for idx in range(M):
     r, c, d, s = map(int, input().split())
     player_grid[r-1][c-1] = idx
-    players.append(Player(idx, r-1, c-1, d, s))
+    positions.append((r-1, c-1))
+    directions.append(d)
+    status.append(s)
 
 # 실행부
 scores = [0] * M
 for _ in range(K):
-    for idx in range(M):
-        players[idx].move()
+    for player in range(M):
+        move(player)
 
 # 정답 출력
 print(*scores)
