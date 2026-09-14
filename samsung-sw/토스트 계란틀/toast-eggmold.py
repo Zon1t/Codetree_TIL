e5b2a2fa9d2177399c6eb152f7d0dc636b8a0aae
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-09-14 17:32:45 +0900

    [Passed] 토스트 계란틀 (296ms, 24MB)

--- samsung-sw/토스트 계란틀/toast-eggmold.py
+++ samsung-sw/토스트 계란틀/toast-eggmold.py
@@ -1,77 +1,71 @@
-# 전형적인 bfs 사용하는 시뮬레이션 문제인 것 같다.
-# 매 회차마다 방문 배열 세팅해두고, 순회하며 미방문인 곳을 시뮬레이션 돌리면 될 것 같다.
-# 현재 위치에 대한 계란 양을 기준으로 뻗어나가는 것이 중요할 것으로 보인다.
-
-from collections import deque
+# 시작 5:17
+# 단지 번호 붙이기 유형 문제. 적절한 조건에 따른 그루핑 + 잘 처리하자.
+# 달리 생각할 부분은 없을 듯
 
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
+
+def in_range(row, col):
+    return 0 <= row < N and 0 <= col < N
+
+
 def bfs(sr, sc):
-    global changed
+    visited[sr][sc] = True
+    total, cnt = egg_grid[sr][sc], 1
+    lst, pointer = [(sr, sc)], 0
 
-    # 계란물의 평균을 계산하기 위함.
-    total = arr[sr][sc]
-    cnt = 1
-
-    # 평균을 적용하기 위함.
-    will_changed = set()
-    will_changed.add((sr, sc))
-
-    Q = deque([(sr, sc)])
-    while Q:
-        curr_row, curr_col = Q.popleft()
+    while pointer < cnt:
+        curr_row, curr_col = lst[pointer]
+        pointer += 1
 
         for d in range(4):
             next_row, next_col = curr_row + dr[d], curr_col + dc[d]
 
-            # 만약 범위에서 벗어나면 스킵
-            if not in_range(next_row, next_col):
-                continue
-            # 만약 이미 방문했으면 스킵
-            if visited[next_row][next_col]:
+            if not in_range(next_row, next_col) or visited[next_row][next_col]:
                 continue
 
-            # 범위 안에 들어온다면 같이 합칠 계란으로 포함시키기.
-            if L <= abs(arr[next_row][next_col] - arr[curr_row][curr_col]) <= R:
+            if L <= abs(egg_grid[curr_row][curr_col]-egg_grid[next_row][next_col]) <= R:
                 visited[next_row][next_col] = True
-                Q.append((next_row, next_col))
+                lst.append((next_row, next_col))
 
-                total += arr[next_row][next_col]
+                total += egg_grid[next_row][next_col]
                 cnt += 1
 
-                will_changed.add((next_row, next_col))
+    if cnt == 1:
+        return False
 
-    # 만약 다른 계란과 합쳐지게 된다면
-    if cnt > 1:
-        average = total // cnt          # 소숫점은 버리므로..
-        for row, col in will_changed:
-            arr[row][col] = average
-
-        changed = True
+    average = total // cnt
+    for gr, gc in lst:
+        egg_grid[gr][gc] = average
+    return True
 
 
-def in_range(row, col):
-    return (0 <= row < N and 0 <= col < N)
+def print_grid():
+    print(f'----{turn}----')
+    for row in egg_grid:
+        print(*row)
+    print()
 
 
 N, L, R = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-chage_cnt = 0
-while True:
-    visited = [[False] * N for _ in range(N)]
+egg_grid = [list(map(int, input().split())) for _ in range(N)]
 
-    changed = False
+turn = 0
+while True:
+    is_changed = False
+    visited = [[False] * N for _ in range(N)]
     for row in range(N):
         for col in range(N):
-            if not visited[row][col]:
-                visited[row][col] = True
-                bfs(row, col)
+            if visited[row][col]:
+                continue
 
-    if not changed:
+            if bfs(row, col):
+                is_changed = True
+
+    if not is_changed:
         break
 
-    # 바뀌었으면 += 1
-    chage_cnt += 1
+    turn += 1
 
-print(chage_cnt)
+print(turn)
