4900e1755907811fd0c70b2e4d929c2872424977
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-09-15 19:44:21 +0900

    [Passed] 원자 충돌 (312ms, 30MB)

--- samsung-sw/원자 충돌/atom-collision.py
+++ samsung-sw/원자 충돌/atom-collision.py
@@ -1,74 +1,84 @@
-# 격자를 벗어난다는 개념은 존재하지 않는다. 다 mod N 처리.
-# 음.. n이 작긴 한데 격자를 그릴 필요가 있나 싶다. 그냥 dictionary 써서 하는 게 마음 편할 것 같다.
-# 만들 함수. 그냥 move, update, calc_scores
+# 7:20 시작
+# 1. 모든 원자는 1초 지날 때마다 본인 방향의 속력만큼 이동.
+# 2. 이동이 끝난 이후, 2개 이상의 원자가 있으면 합성이 일어남. -> 조건 참조.
+# move -> boom 정도로 구성하면 되겠다. 좌표 모두 %N 처리 잘하기. 그리드는 매번 업데이트 해주기.
+# 나머진 크게 고려할 사항이 없을듯..?
+
 
 dr = [-1, -1, 0, 1, 1, 1, 0, -1]
 dc = [0, 1, 1, 1, 0, -1, -1, -1]
 
+
 def move():
-    temp = dict()
-    for row, col, mass, speed, direction in data:
-        next_row, next_col = (row + speed*dr[direction])%N, (col + speed*dc[direction])%N
+    return_dict = dict()
+    for curr_row, curr_col in list(data.keys()):
+        for curr_mass, curr_speed, curr_dir in data[(curr_row, curr_col)]:
+            next_row, next_col = (curr_row + dr[curr_dir] * curr_speed) % N, (curr_col + dc[curr_dir] * curr_speed) % N
 
-        if (next_row, next_col) in temp:
-            temp[(next_row, next_col)][0] += mass
-            temp[(next_row, next_col)][1] += speed
-            temp[(next_row, next_col)][2] += 1
-            temp[(next_row, next_col)][3].append(direction)
+            if (next_row, next_col) in return_dict:
+                return_dict[(next_row, next_col)][0] += curr_mass
+                return_dict[(next_row, next_col)][1] += curr_speed
+                return_dict[(next_row, next_col)][2] += 1
+                return_dict[(next_row, next_col)][3].append(curr_dir)
+            else:
+                return_dict[(next_row, next_col)] = [curr_mass, curr_speed, 1, [curr_dir]]
+    return return_dict
 
+
+def boom():
+    return_data = dict()
+    for curr_row, curr_col in list(new_data.keys()):
+        total_mass, total_speed, total_cnt, dir_lst = new_data[(curr_row, curr_col)]
+        if total_cnt == 1:
+            return_data[(curr_row, curr_col)] = [(total_mass, total_speed, dir_lst[0])]
         else:
-            temp[(next_row, next_col)] = [mass, speed, 1, [direction]]
+            next_mass, next_speed = total_mass // 5, total_speed // total_cnt
 
-    return temp
-
-
-def update():
-    global data
-
-    new_data = []
-    for (row, col), (mass, speed, cnt, direction) in atom_dict.items():
-        if cnt == 1:
-            new_data.append((row, col, mass, speed, direction[0]))
-        else:
-            next_mass, next_speed = mass // 5, speed // cnt
-
-            if next_mass == 0:
+            if not next_mass:
                 continue
 
-            # 다음 어떻게 퍼져나갈지 연산하기.
-            now_state = direction[0] % 2
-            for d in direction[1:]:
-                if now_state != d % 2:
-                    now_state = 2
+            standard = dir_lst[0]%2
+            for i in range(1, total_cnt):
+                if standard != dir_lst[i]%2:
+                    standard = 2
                     break
 
-            # 상태에 맞게 퍼뜨리기.
-            for d in ((0, 2, 4, 6) if now_state != 2 else (1, 3, 5, 7)):
-                new_data.append((row, col, next_mass, next_speed, d))
+            return_data[(curr_row, curr_col)] = []
+            for next_dir in ([1, 3, 5, 7] if standard == 2 else [0, 2, 4, 6]):
+                return_data[(curr_row, curr_col)].append((next_mass, next_speed, next_dir))
+    return return_data
 
-    # data 업데이트 해주기.
-    data = new_data
 
-def calc_scores():
+def get_answer():
     temp = 0
-    for _, _, m, _, _ in data:
-        temp += m
+    for row, col in data.keys():
+        for mass, _, _ in data[(row, col)]:
+            temp += mass
     return temp
 
 
-# 헷갈릴 것 같아서 자주 쓰는 변수로 바꿔 받았다.
-N, K, T = map(int, input().split())
+def custom_print():
+    for row, col in data.keys():
+        print(f'----row:{row}, col:{col}----')
+        for mass, speed, d in data[(row, col)]:
+            print(mass, speed, d)
 
-# 기본적으로 이동에 대한 정보를 기록하는 변수. 지속적으로 업데이트 될 것.
-data = []
-for _ in range(K):
+
+N, M, K = map(int, input().split())
+data = dict()
+
+for _ in range(M):
     r, c, m, s, d = map(int, input().split())
-    data.append((r-1, c-1, m, s, d))
+    data[(r-1, c-1)] = [(m, s, d)]
 
-for _ in range(T):
-    # 기록용으로 남기는 딕셔너리. atom_dict[(row, col)] = [mass, speed, cnt, directions]
-    atom_dict = move()
-    update()
+# 실행부
+for _ in range(K):
+    # 1. 움직이자.
+    new_data = move()
 
-answer = calc_scores()
+    # 2. 처리하자.
+    data = boom()
+
+# 정답 출력
+answer = get_answer()
 print(answer)
