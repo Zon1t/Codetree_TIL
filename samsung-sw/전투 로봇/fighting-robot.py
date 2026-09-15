d6810c8415d24f0df7839658be6aff5405167e68
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-09-15 20:12:47 +0900

    [Passed] 전투 로봇 (56ms, 17MB)

--- samsung-sw/전투 로봇/fighting-robot.py
+++ samsung-sw/전투 로봇/fighting-robot.py
@@ -1,58 +1,85 @@
+# 7:55 시작
+# 대상 우선순위 (거리, 행, 열) << 잘 지켜서 bfs하면 문제 없이 통과할 것.
+# 레벨업 처리 잘하기. 레벨에 따른 처리 적절하게 잘하기.
+
 from collections import deque
 
-N = int(input())
-end = N**2
-deltas = (1, -1, N, -N)
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
 
-pos = []
-curr_pos = -1
-for i in range(N):
-    temp = list(map(int, input().split()))
-    if curr_pos == -1:
-        for j in range(N):
-            if temp[j] == 9:
-                curr_pos = i*N+j
-                temp[j] = 0
-    pos.extend(temp)
 
-def bfs():
-    visited = 1<<curr_pos
-    time_ = 0
-    monsters = []
-    Q = deque()
-    Q.append((0, curr_pos))
+def in_range(row, col):
+    return 0 <= row < N and 0 <= col < N
+
+
+def find():
+    visited = [[-1] * N for _ in range(N)]
+    visited[robot_row][robot_col] = 0
+
+    find = []
+    Q = deque([(robot_row, robot_col)])
     while Q:
-        now_time, now_pos = Q.popleft()
-        if time_ != now_time:
-            if monsters:
-                monsters.sort()
-                return monsters[0], now_time
-            time_ = now_time
-        for d in deltas:
-            next_pos = now_pos + d
-            if not (0 <= next_pos < end) or (visited & 1<<next_pos):
-                continue
-            if (d==1 and next_pos%N==0) or (d==-1 and next_pos%N==N-1):
-                continue
-            if pos[next_pos] > level:
-                continue
-            elif 0 < pos[next_pos] < level:
-                monsters.append(next_pos)
-            visited |= 1 << next_pos
-            Q.append((now_time+1, next_pos))
-    return -1, -1
+        for _ in range(len(Q)):
+            curr_row, curr_col = Q.popleft()
+            for d in range(4):
+                next_row, next_col = curr_row + dr[d], curr_col + dc[d]
+                if not in_range(next_row, next_col) or grid[next_row][next_col] > level:
+                    continue
+                if visited[next_row][next_col] != -1:
+                    continue
 
-time, cnt, level = 0, 0, 2
+                if 0 < grid[next_row][next_col] < level:
+                    find.append((next_row, next_col))
+
+                visited[next_row][next_col] = visited[curr_row][curr_col] + 1
+                Q.append((next_row, next_col))
+
+        if find:
+            find.sort()
+            return_row, return_col = find[0]
+            return return_row, return_col, visited[return_row][return_col]
+
+    return -1, -1, -1
+
+
+def print_grid():
+    print(f'----grid----')
+    for row in grid:
+        print(*row)
+
+
+def print_status():
+    print(f'----status----')
+    print(robot_row, robot_col, answer)
+
+
+N = int(input())
+grid = [list(map(int, input().split())) for _ in range(N)]
+
+robot_row, robot_col = -1, -1
+for row in range(N):
+    for col in range(N):
+        if grid[row][col] == 9:
+            robot_row, robot_col = row, col
+            grid[row][col] = 0
+
+level, cnt = 2, 0
+
+answer = 0
 while True:
-    next_pos, delta = bfs()
-    if next_pos == -1:
+    monster_row, monster_col, time = find()
+
+    if time == -1:
         break
 
-    curr_pos = next_pos
-    pos[curr_pos] = 0
-    time += delta
+    answer += time
     cnt += 1
     if cnt == level:
         level += 1
         cnt = 0
-print(time)
+
+    robot_row, robot_col = monster_row, monster_col
+    grid[robot_row][robot_col] = 0
+
+
+print(answer)
