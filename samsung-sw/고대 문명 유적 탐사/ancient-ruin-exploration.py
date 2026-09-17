d24385d5d537ffcf5039aa7700d9a9e2b6a3cc17
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-09-17 21:20:29 +0900

    [Passed] 고대 문명 유적 탐사 (160ms, 22MB)

--- samsung-sw/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ samsung-sw/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -1,7 +1,4 @@
-# 큐 안 쓰는 게 더 낫나?
-
-from collections import deque
-
+# 큐 안 쓰는 버전
 
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
@@ -12,121 +9,114 @@ def in_range(row, col):
 
 
 def bfs(find_grid):
-    Q = deque()
-    return_value = 0
+    return_lst = []
     visited = [[False] * 5 for _ in range(5)]
     for row in range(5):
         for col in range(5):
             if visited[row][col]:
                 continue
 
-            temp = 0
             visited[row][col] = True
 
-            Q.append((row, col))
-            while Q:
-                curr_row, curr_col = Q.popleft()
-                temp += 1
-
+            lst = [(row, col)]
+            pointer, cnt = 0, 1
+            while pointer < cnt:
                 for d in range(4):
-                    next_row, next_col = curr_row + dr[d], curr_col + dc[d]
+                    next_row, next_col = lst[pointer][0] + dr[d], lst[pointer][1] + dc[d]
 
                     if not in_range(next_row, next_col) or visited[next_row][next_col]:
                         continue
-                    if find_grid[next_row][next_col] != find_grid[curr_row][curr_col]:
+                    if find_grid[next_row][next_col] != find_grid[lst[pointer][0]][lst[pointer][1]]:
                         continue
 
                     visited[next_row][next_col] = True
-                    Q.append((next_row, next_col))
+                    lst.append((next_row, next_col))
+                    cnt += 1
 
-            if temp < 3:
+                pointer += 1
+
+            if cnt < 3:
                 continue
-            return_value += temp
+            else:
+                return_lst += lst
 
-    return return_value
+    return return_lst, len(return_lst)
 
 
-def swap(*lst, reverse=False):
-    if reverse: return (lst[-1],)+lst[:-1]
-    else: return lst[1:]+(lst[0],)
+def small_bfs(lst):
+    visited = [[False] * 5 for _ in range(5)]
+    return_lst = []
+    for row, col in lst:
+        if visited[row][col]:
+            continue
+
+        visited[row][col] = True
+        pointer, cnt = 0, 1
+
+        Q = [(row, col)]
+        while pointer < cnt:
+            for d in range(4):
+                next_row, next_col = Q[pointer][0] + dr[d], Q[pointer][1] + dc[d]
+                if not in_range(next_row, next_col) or visited[next_row][next_col]:
+                    continue
+                if grid[next_row][next_col] != grid[Q[pointer][0]][Q[pointer][1]]:
+                    continue
+
+                visited[next_row][next_col] = True
+                Q.append((next_row, next_col))
+                cnt += 1
+            pointer += 1
+
+        if cnt < 3:
+            continue
+        else:
+            return_lst += Q
+
+    return return_lst
+
 
 
 def rotate(sr, sc, cnt_):
+    temp_grid = [row[sc:sc + 3] for row in grid[sr:sr + 3]]
+    if cnt_ == 1:
+        temp_grid = [row[::-1] for row in zip(*temp_grid)]
+    elif cnt_ == 2:
+        temp_grid = [row[::-1] for row in temp_grid[::-1]]
+    else:
+        temp_grid = [row[:] for row in zip(*temp_grid)][::-1]
+
     return_grid = [row[:] for row in grid]
 
-    if cnt_ == 2:
-        return_grid[sr][sc], return_grid[sr+2][sc+2] = swap(return_grid[sr][sc], return_grid[sr+2][sc+2])
-        return_grid[sr][sc+1], return_grid[sr+2][sc+1] = swap(return_grid[sr][sc+1], return_grid[sr+2][sc+1])
-        return_grid[sr][sc+2], return_grid[sr+2][sc] = swap(return_grid[sr][sc+2], return_grid[sr+2][sc])
-        return_grid[sr+1][sc+2], return_grid[sr+1][sc] = swap(return_grid[sr+1][sc+2], return_grid[sr+1][sc])
-    else:
-        return_grid[sr][sc], return_grid[sr+2][sc], return_grid[sr+2][sc+2], return_grid[sr][sc+2] = \
-            swap(return_grid[sr][sc], return_grid[sr+2][sc], return_grid[sr+2][sc+2], return_grid[sr][sc+2], reverse=cnt_==3)
-        return_grid[sr][sc+1], return_grid[sr+1][sc], return_grid[sr+2][sc+1], return_grid[sr+1][sc+2] = \
-            swap(return_grid[sr][sc+1], return_grid[sr+1][sc], return_grid[sr+2][sc+1], return_grid[sr+1][sc+2], reverse=cnt_==3)
+    for delta_row in range(3):
+        for delta_col in range(3):
+            return_grid[sr + delta_row][sc + delta_col] = temp_grid[delta_row][delta_col]
 
     return return_grid
 
 
 def priority():
     curr_max = (0, 0, 0, 0)
+    max_lst, max_grid = None, None
     for row in range(3):
         for col in range(3):
             for cnt in range(1, 4):
                 rotate_grid = rotate(row, col, cnt)
-                money = bfs(rotate_grid)
+                pos_lst, money = bfs(rotate_grid)
 
                 if curr_max < (money, -cnt, -col, -row):
                     curr_max = (money, -cnt, -col, -row)
-
-    return (-curr_max[1], -curr_max[2], -curr_max[3]) if curr_max[0] != 0 else (-1, -1, -1)
-
-
-def get_money():
-    total_money = 0
-    visited = [[False] * 5 for _ in range(5)]
-    for row in range(5):
-        for col in range(5):
-            if visited[row][col]:
-                continue
-
-            visited[row][col] = True
-            cnt, pointer = 1, 0
-
-            check_lst = [(row, col)]
-            while pointer < cnt:
-                curr_row, curr_col = check_lst[pointer]
-                pointer += 1
-
-                for d in range(4):
-                    next_row, next_col = curr_row + dr[d], curr_col + dc[d]
-                    if not in_range(next_row, next_col) or visited[next_row][next_col]:
-                        continue
-                    if grid[next_row][next_col] != grid[curr_row][curr_col]:
-                        continue
-                    visited[next_row][next_col] = True
-                    check_lst.append((next_row, next_col))
-                    cnt += 1
-
-            if cnt < 3:
-                continue
-
-            total_money += cnt
-            for row, col in check_lst:
-                grid[row][col] = 0
-
-    return total_money
+                    max_grid = rotate_grid
+                    max_lst = pos_lst
+    
+    return max_lst, max_grid, -curr_max[3], -curr_max[2] 
 
 
-def fill():
+def get_money(lst):
     global fill_pointer
-    for col in range(5):
-        for row in range(4, -1, -1):
-            if grid[row][col]:
-                continue
-
-            grid[row][col] = fill_nums[fill_pointer]
-            fill_pointer += 1
+    for row, col in lst:
+        grid[row][col] = fill_nums[fill_pointer]
+        fill_pointer += 1
+    return len(lst)
 
 
 def custom_print():
@@ -143,33 +133,35 @@ grid = [list(map(int, input().split())) for _ in range(5)]
 
 fill_nums = list(map(int, input().split()))
 fill_pointer = 0
+ordering_rule = lambda x: (x[1], -x[0])
 
 # ==============================================================
 # 실행부
 
 answer = []
 for _ in range(K):
-    # 1. 회전할 부분 찾기.
-    cnt, rotate_col, rotate_row = priority()
+    # 1. 우선 순위에 의거한 회전 시키기.
+    lst, grid, rotate_col, rotate_row = priority()
     # ** 종료 체크하기.
-    if cnt == -1:
+    if lst is None:
         break
 
-    # 2. 회전시키기.
-    grid = rotate(rotate_row, rotate_col, cnt)
+    # 2. 유물 획득하기.
+    lst.sort(key=ordering_rule)
+    curr_money = get_money(lst)
 
-    # 3. 유물 획득하기.
-    total_earn = 0
+    # 3. 연쇄작용 처리.
     while True:
-        money = get_money()
+        lst = small_bfs(lst)
 
-        if not money:
+        if lst:
+            lst.sort(key=ordering_rule)
+            curr_money += get_money(lst)
+        else:
             break
 
-        total_earn += money
-        fill()
-
     # 4. 정답 기록하기.
-    answer.append(total_earn)
+    answer.append(curr_money)
 
+# 정답 출력
 print(*answer)
