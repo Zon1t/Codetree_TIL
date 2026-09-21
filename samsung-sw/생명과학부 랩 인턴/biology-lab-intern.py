a47a21e44ac78a906347a580711145733839c0a5
Zon1t <137697422+Zon1t@users.noreply.github.com>
2026-09-21 11:25:13 +0900

    [Passed] 생명과학부 랩 인턴 (286ms, 25MB)

--- samsung-sw/생명과학부 랩 인턴/biology-lab-intern.py
+++ samsung-sw/생명과학부 랩 인턴/biology-lab-intern.py
@@ -1,128 +1,98 @@
-''' 생명과학부 랩 인턴 / 20260901 / 체감 난이도 : 골드 5
-소요 시간 : 38분 / 시도 : 1회 / 실행 시간 : 274ms / 메모리 : 27MB
-
-녹화가 안눌러져 있었습니다.. 쉽게쉽게 해결한 문제였어서 그나마 다행인 것 같긴 합니다..
-다음부터는 녹화 체크 꼭 하기!
-
-
-[구상]
-    - 사실 너무 익숙한 문제라 데이터를 어떻게 관리해야 할지 머리에 금방금방 정리되었던 것 같습니다.
-    - 수행해야 하는 기능들도 단순하다고 생각했어서 함수나 변수를 금방 정리하고 구현 단계로 넘어갔습니다.
-
-[구현]
-    - 구상 때와 일부 달라진 점이 있습니다. 막상 column 기준으로 실행을 하자니, grid를 만들지 않은
-    풀이 방식에서 새로 dictionary를 만들까 고민을 했던 것 같습니다. dict_lst[col][row]처럼 col
-    단위로 딕셔너리를 만드는 등? 해법을 떠올렸는데, 어짜피 move함수에서 좌표를 계산할 테니, 거기서
-    다음 column에 있는 row들을 뽑아오자 생각이 들었습니다. row만 뽑아와도 dictionary 키값 접근이
-    가능했기에 해당 부분은 잘한 부분이라 생각합니다.
-    - 이동 규칙을 개미 때와 동일하다고 생각했는데, 막상 구현하려니 다르다는 사실을 알게 되었습니다.
-    반사되는 기준이 개미 때는 벽이였고, 여기선 마지막 칸이다 보니 살짝 헷갈리는 구석이 있었습니다. 더
-    불어 방향에 대해서도 결정의 해줘야 하다보니 다시 규칙성을 발견해야 했습니다. 다행히 전체를 그려보고,
-    늘려보고 하면서 관찰하니 곧바로 규칙이 보였고 해당 로직을 금방 구현할 수 있었습니다.
-
-[검증]
-    - 매 순간 올바른 위치에 올바른 방향으로 존재하고, 먹는 로직이 정상적으로 동작함을 출력해보며 확인
-    했습니다. 이후 문제와 다시 교차 검증을 해보고 제출했습니다.
-
-'''
-
-# 1. 열별 탐색 진행. 이때 채취 여부를 체크하면 좋을 것 같다.
-# 2. 이동 진행. 이때 위치는 어떻게 연산하는가? 개미 문제를 떠올리면 너무 쉽게 구할 수 있다.
-# 2-1. 만약 같은 공간에 곰팡이가 있으면 실시간으로 잡아먹게끔 세팅해주면 될 것이다.
-# 3. 모든 열에 대해서 위 과정을 수행하고, 끝났을 때의 곰팡이 크기 총합 출력하기.
-# 좌표랑 방향 모두 1부터 시작함에 유의하자.
-# 딕셔너리 쓰면 금방 풀 수 있지 않을까?
+# 10:50시작    /[
+# 열 순차 탐색. 빨리 발견한 곰팡이를 채취한다. 물론 못 할 수도 있음
+# 곰팡이 이동 로직 잘 설계하기. 주기 : 2N - 2. 속력까지 고려하기
+# 같은 칸에 있으면 잡아먹기. 크기는 모두 다름.
+# 채취한 곰팡이의 총합을 구하는 문제. 채취할 떄마다 업뎃. 다음 칸 곰팡이는 미리 담아두는 방식 채용
 
 
-# 순서대로 위, 아래, 오른, 왼
-dr = [-1, 1, 0, 0]
-dc = [0, 0, 1, -1]
+class Gompang:
 
+    DELTA_R = [-1, 1, 0, 0]
+    DELTA_C = [0, 0, 1, -1]
 
-# 해당 위치에 있는 곰팡이를 채취하는 함수.
-def catch(row, col):
-    global answer
-    _, _, size = gompang2.pop((row, col))
-    answer += size
+    def __init__(self, i, r, c, s, d, b):
+        self.idx = i
+        self.row = r
+        self.col = c
+        self.speed = s
+        self.direction = d
+        self.size = b
+        self.alive = True
 
-# 곰팡이를 이동시켜주자!
-def move():
-    global gompang2
-    # 이동 이후 정보를 저장할 딕셔너리.
-    new_pang2 = dict()
-    # 다음 열에 있는 애들을 미리 저장해주기 위해 비워두기.
-    catch_pang2.clear()
+    def update(self, r, c):
+        target_idx = grid[r][c]
 
-    # 존재하는 곰팡이들에 대해 순회.
-    for curr_row, curr_col in gompang2:
-        speed, curr_dir, size = gompang2[(curr_row, curr_col)]
-
-        # 위/아래로 움직이는 경우
-        if curr_dir <= 1:
-            # 반복 주기 : 2N-2. 이를 활용해 다음 좌표 및 방향 연산.
-            next_row = (curr_row + speed * dr[curr_dir]) % row_standard
-            next_col = curr_col
-            if next_row <= N-1:
-                next_dir = curr_dir
-            else:
-                next_row = row_standard-next_row
-                next_dir = curr_dir ^ 1
-        # 좌/우로 움직이는 경우
+        if target_idx == -1 or pang_lst[target_idx].size < self.size:
+            self.row = r
+            self.col = c
+            if target_idx != -1:
+                pang_lst[target_idx].alive = False
+            grid[r][c] = self.idx
+            return r, c
         else:
-            # 반복 주기 : 2M-2. 이를 활용해 다음 좌표 및 방향 연산.
-            next_row = curr_row
-            next_col = (curr_col + speed * dc[curr_dir])%col_standard
-            if next_col <= M-1:
-                next_dir = curr_dir
-            else:
-                next_col = col_standard-next_col
-                next_dir = curr_dir ^ 1
-        
-        # 만약 다음 채취 대상이 되는 column에 있다면 기록해주자.
-        if next_col == col+1:
-            catch_pang2.append(next_row)
-        
-        # 만약 해당 위치에 이미 곰팡이가 있으면, 크기에 따라 업데이트 해주기.
-        if (next_row, next_col) in new_pang2:
-            if size > new_pang2[(next_row, next_col)][2]:
-                new_pang2[(next_row, next_col)] = (speed, next_dir, size)
-        # 없으면 그냥 적기
-        else:
-            new_pang2[(next_row, next_col)] = (speed, next_dir, size)
+            self.alive = False
+            return None, None
 
-    # 업데이트
-    gompang2 = new_pang2
+    def move(self):
+        next_row = (self.row + self.DELTA_R[self.direction] * self.speed) % sero
+        next_col = (self.col + self.DELTA_C[self.direction] * self.speed) % garo
+
+        if next_row >= N:
+            next_row = sero - next_row
+            self.direction ^= 1
+        if next_col >= M:
+            next_col = garo - next_col
+            self.direction ^= 1
+
+        return self.update(next_row, next_col)
 
 
-# 입력받기
+def print_grid(grid):
+    print(f'----pang_grid----')
+    for row in grid:
+        print(*row)
+
+
+# =====================================================
+# 세팅
+
 N, M, K = map(int, input().split())
-
-# 움직이는 로직에서 사용할 변수들.
-row_standard, col_standard = 2*N-2, 2*M-2
-
-# 기록용 변수
-gompang2, catch_pang2 = dict(), []
-for _ in range(K):
+pang_gird = [[-1] * M for _ in range(N)]
+next_target = None
+garo, sero = 2*M-2, 2*N-2
+pang_lst = []
+for idx in range(K):
     r, c, s, d, b = map(int, input().split())
-    r, c, d = r-1, c-1, d-1
-    
-    # 순회를 또 하긴 싫으니 미리 저장해두고 꺼내 쓰자.
-    if c == 0:
-        catch_pang2.append(r)
-    
-    # data 저장.
-    gompang2[(r, c)] = (s, d, b)
+    pang_gird[r-1][c-1] = idx
+    pang_lst.append(Gompang(idx, r-1, c-1, s, d-1, b))
+    if c == 1 and (next_target is None or (r-1, idx) < next_target):
+        next_target = (r-1, idx)
 
-# 채취할 때마다 업뎃 ㄱㄱ
 answer = 0
-# 실험 진행
+
+# =====================================================
+# 실행부
+
 for col in range(M):
-    # 1. 곰팡이 채취. 해당 열에 채취할 곰팡이가 있으면 채취해주자.
-    if catch_pang2:
-        catch_pang2.sort()
-        catch(catch_pang2[0], col)
+    if next_target is not None:
+        _, target_idx = next_target
+        answer += pang_lst[target_idx].size
+        pang_lst[target_idx].alive = False
+        next_target = None
 
-    # 2. 곰팡이 움직이기.
-    move()
+    grid = [[-1] * M for _ in range(N)]
+    for pang in pang_lst:
+        if not pang.alive:
+            continue
 
+        pr, pc = pang.move()
+        if pr is None:
+            continue
+
+        if pc == col+1 and (next_target is None or pr <= next_target[0]):
+            next_target = (pr, pang.idx)
+
+    pang_gird = grid
+
+# 정답 출력
 print(answer)
